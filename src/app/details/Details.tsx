aae96419301f73b5385407c29534d5ffc6f75d67
Nino Khurtsidze <103818368+Ninunutsi@users.noreply.github.com>
2024-03-16 16:10:54 +0400

    details design

--- src/app/details/Details.tsx
+++ src/app/details/Details.tsx
@@ -1,24 +1,49 @@
 'use client'
 
-import { Alert, Box, Typography } from '@mui/material'
+import { Alert, Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, styled } from '@mui/material'
 import React from 'react'
 import { useMode } from '../context/QuizModeCotext';
 import Link from 'next/link';
+import BackgroundImg from '../components/bgImage/BackgroundImg';
+
+
+const StyledTableCell = styled(TableCell)(() => ({
+  fontSize: 18,
+  fontWeight: 'bold'
+}));
+
 
 const Details = () => {
     const { mode, valueArr, time } = useMode();
   return (
-    <Box>
-        <Link href="/results">go back</Link>
+    <Box color={mode ? "black" : "white"}>
+       <Link href="/results"><Typography mt={5} sx={{fontSize: '1.2rem', cursor: 'pointer', paddingLeft: 10}} color={mode ? "black" : "white"}>go back</Typography></Link>
+    <BackgroundImg />
 
-      <Box>
-        Time taken of each section:
-        <Typography>HTML: {time.html}</Typography>
-        <Typography>CSS: {time.css}</Typography>
-        <Typography>JAVASCRIPT: {time.javascript}</Typography>
-        <Typography>ACCESSEBILITY: {time.accessibility}</Typography>
-      </Box>
-    {valueArr.map(({ correct, checked }, index) => (
+    <Typography textAlign={"center"} mb={4} fontSize={48} mt={5}>
+        Your Details
+      </Typography>
+      <TableContainer component={Paper} sx={{minWidth: 650, maxWidth: 1000, margin: "auto", marginBottom: 10}}>
+      <Table sx={{ minWidth: 650, maxWidth: 1000}} aria-label="simple table">
+        <TableHead>
+          <TableRow sx={{fontWeight: "bold", color: "red"}}>
+            <StyledTableCell>HTML</StyledTableCell>
+            <StyledTableCell>CSS</StyledTableCell>
+            <StyledTableCell>JavaScript</StyledTableCell>
+            <StyledTableCell>Accessibility</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+          <TableCell>{time.html}</TableCell>
+          <TableCell>{time.css}</TableCell>
+          <TableCell>{time.javascript}</TableCell>
+          <TableCell>{time.accessibility}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+    {valueArr.map(({ correct, checked, }, index) => (
       <Typography
         key={index}
         display={"flex"}
